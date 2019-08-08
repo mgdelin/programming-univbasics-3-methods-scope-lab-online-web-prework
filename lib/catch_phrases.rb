957b4ef42a9c7003a5bdb1033fbb7eacda77079e
remote-anchor-6652 <mercedesgdelin@gmail.com>
2019-08-08 19:33:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 
 def mario
   puts phrase
-  phrase = "'It's-a me, Mario!'"
+phrase = "'It's-a me, Mario!'"
 end
 
 def toadstool
   puts status
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
